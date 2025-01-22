a7aaae0639e9e0095ae70cf2212ee47d10752d02
Djizus <mehrcj@gmail.com>
2025-01-22 13:21:39 +0100

    Fixing deployement issues

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ interface DiscordGuild {
   permissions: string;
 }
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: mongoAdapter,
   providers: [
     DiscordProvider({
@@ -63,4 +63,6 @@ export const authOptions: AuthOptions = {
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST } 
+
+export { handler as GET, handler as POST }
+export { authOptions } 
